c9c613830899ec9091a0244c40b1abd606030ee8
haseebc <haseebchaudhary@gmail.com>
2020-06-02 14:44:15 +0200

    simple landing page built

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-  def home
-  end
+  def home; end
+
+  def glossary; end
+
+  def healthcheck; end
+
+  def privacy; end
+
+  def blog; end
+
+  def about; end
+  
 end
